974558d3ad187247fb7e7fd7cc0ca5e07499b3ed
AbabilGroup <itek.department@gmail.com>
2025-02-26 18:22:26 +0600

    contact button link

--- src/components/pages/homepage/Contact.tsx
+++ src/components/pages/homepage/Contact.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Contact = () => {
   return (
@@ -6,9 +7,11 @@ const Contact = () => {
       <div className="container space-y-5">
         <h2 className="text-center text-4xl font-bold">Contact us.</h2>
         <div className="flex items-center justify-center">
-          <Button className="rounded-full bg-primary-pi px-10 py-8 shadow-none hover:bg-[#7A7A7A]">
-            SEND AN INQUIRY
-          </Button>
+          <Link href="/contact">
+            <Button className="rounded-full bg-primary-pi px-10 py-8 shadow-none hover:bg-[#7A7A7A]">
+              SEND AN INQUIRY
+            </Button>
+          </Link>
         </div>
       </div>
     </section>
